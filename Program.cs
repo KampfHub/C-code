8f1351e23746f85d720d7458862e9ef5a2a1ca26
KampfHub <108213505+KampfHub@users.noreply.github.com>
2022-06-26 10:50:50 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -12,20 +12,20 @@ double crystalPoint4 = 250;
 double crystalPoint5 = 1000;
 double crystalPoint6 = 1500;
 Console.WriteLine("Enter the quantity of gold");
-double currentGold = Convert.ToDouble(Console.ReadLine());
+double purchasedGold = Convert.ToDouble(Console.ReadLine());
 
-if (currentGold == goldPoint1) receivedCrystal = crystalPoint1;
-if (currentGold <= goldPoint2) receivedCrystal = crystalPoint2;
-if (currentGold <= goldPoint3 & currentGold > goldPoint2) receivedCrystal = getGold(crystalPoint2,crystalPoint3,goldPoint2, goldPoint3, currentGold);
-if (currentGold <= goldPoint4 & currentGold > goldPoint3) receivedCrystal = getGold(crystalPoint3, crystalPoint4, goldPoint3, goldPoint4, currentGold);
-if (currentGold <= goldPoint5 & currentGold > goldPoint4) receivedCrystal = getGold(crystalPoint4, crystalPoint5, goldPoint4, goldPoint5, currentGold);
-if (currentGold > goldPoint5) receivedCrystal = getGold(crystalPoint5, crystalPoint6, goldPoint5, goldPoint6, currentGold);
+if (purchasedGold == goldPoint1) receivedCrystal = crystalPoint1;
+if (purchasedGold <= goldPoint2) receivedCrystal = crystalPoint2;
+if (purchasedGold <= goldPoint3 & purchasedGold > goldPoint2) receivedCrystal = getGold(crystalPoint2,crystalPoint3,goldPoint2, goldPoint3, purchasedGold);
+if (purchasedGold <= goldPoint4 & purchasedGold > goldPoint3) receivedCrystal = getGold(crystalPoint3, crystalPoint4, goldPoint3, goldPoint4, purchasedGold);
+if (purchasedGold <= goldPoint5 & purchasedGold > goldPoint4) receivedCrystal = getGold(crystalPoint4, crystalPoint5, goldPoint4, goldPoint5, purchasedGold);
+if (purchasedGold > goldPoint5) receivedCrystal = getGold(crystalPoint5, crystalPoint6, goldPoint5, goldPoint6, purchasedGold);
 
 Console.WriteLine("Received Crystal = ");
 Console.WriteLine(Math.Round(receivedCrystal));
 
 double getGold(double crystalBottom, double crystalTop, double goldBottom, double goldTop, double currentGold)
 {
-    double summaryGold = (crystalTop - crystalBottom)/(goldTop-goldBottom) * (currentGold - goldBottom) + crystalBottom;
-    return summaryGold;
+    double totalGold = (crystalTop - crystalBottom)/(goldTop-goldBottom) * (currentGold - goldBottom) + crystalBottom;
+    return totalGold;
 }
